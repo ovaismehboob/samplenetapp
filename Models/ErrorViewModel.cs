0f65674a0e2d9bfcd375b66a902d5e48d9c61a36
Ovais Mehboob Ahmed Khan <Ovais.Mehboob@microsoft.com>
2023-09-19 13:50:20 +0400

    added initial app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace automateddeployments.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
